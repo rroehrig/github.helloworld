26e153e13768c8e0e3b7f025c71a403bffcf1f5d
Richard Steuer <richard.steuer@andrena.de>
2014-04-28 15:52:57 +0200

    Test-Refactoring

--- test/github/helloworld/RomanNumberConverterTest.java
+++ test/github/helloworld/RomanNumberConverterTest.java
@@ -10,17 +10,9 @@ public class RomanNumberConverterTest {
 	private RomanNumberConverter converter = new RomanNumberConverter();
 
 	@Test
-	public void testRomanOne() throws Exception {
+	public void testRomanLiterals() throws Exception {
 		assertThat(converter.convert("I"), is(1));
-	}
-
-	@Test
-	public void testRomanTwo() throws Exception {
 		assertThat(converter.convert("II"), is(2));
-	}
-
-	@Test
-	public void testRomanFive() throws Exception {
 		assertThat(converter.convert("V"), is(5));
 	}
 
