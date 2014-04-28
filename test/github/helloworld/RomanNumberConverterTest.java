9506eeb1aad9624d5145496dfb7d336c0707ee99
rroehrig <rouven.roehrig@andrena.de>
2014-04-28 15:42:48 +0200

    Test code refactored.

--- test/github/helloworld/RomanNumberConverterTest.java
+++ test/github/helloworld/RomanNumberConverterTest.java
@@ -7,21 +7,20 @@ import org.junit.Test;
 
 public class RomanNumberConverterTest {
 
+	private RomanNumberConverter converter = new RomanNumberConverter();
+
 	@Test
 	public void testRomanOne() throws Exception {
-		RomanNumberConverter converter = new RomanNumberConverter();
 		assertThat(converter.convert("I"), is(1));
 	}
 
 	@Test
 	public void testRomanTwo() throws Exception {
-		RomanNumberConverter converter = new RomanNumberConverter();
 		assertThat(converter.convert("II"), is(2));
 	}
 
 	@Test
 	public void testRomanFive() throws Exception {
-		RomanNumberConverter converter = new RomanNumberConverter();
 		assertThat(converter.convert("V"), is(5));
 	}
 
