9b6f7978cb0148868d1de4ffc7123d27f073359d
rroehrig <rouven.roehrig@andrena.de>
2014-04-28 15:45:49 +0200

    Test case for roman three is green.

--- src/github/helloworld/RomanNumberConverter.java
+++ src/github/helloworld/RomanNumberConverter.java
@@ -2,11 +2,13 @@ package github.helloworld;
 
 public class RomanNumberConverter {
 
-	public Integer convert(String string) {
-		if (string.equals("I"))
+	public Integer convert(String romanNumerals) {
+		if (romanNumerals.equals("I"))
 			return 1;
-		else if (string.equals("II"))
+		else if (romanNumerals.equals("II"))
 			return 2;
+		else if (romanNumerals.equals("III"))
+			return 3;
 		return 5;
 	}
 }
